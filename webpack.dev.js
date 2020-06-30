62daae7f5d1882695572b1359f440cc5574c4711
Docterryome Wilkins <docterryomewilkins@DocterryomesAir.attlocal.net>
2020-06-29 19:36:51 -0500

    change all css to sass

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -9,13 +11,16 @@ module.exports = {
     devtool: 'source-map',
     stats: 'verbose',
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+        rules: [{
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        },
+        {
+            test: /\.scss$/,
+            loader:["style-loader", "css-loader", "sass-loader"]
+        }        
+    ]
     },
     plugins: [
         new HtmlWebPackPlugin({
